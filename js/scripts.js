c13865ea385428b9f2eb9a7dffcb0c061a10e533
Austin Lords <austinlords@outlook.com>
2019-02-04 16:24:03 -0800

    add buttons to select background colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,34 @@
 $(function () {
+
   $(".show").click(function() {
-    $(this).siblings(".initially-hidden").toggle();
+    $(this).siblings(".initially-hidden").slideToggle();
     $(this).toggle();
   });
 
   $(".hide").click(function() {
     $(this).siblings(".initially-showing").toggle();
     $(this).toggle();
-    $(this).siblings(".initially-hidden").toggle();
+    $(this).siblings(".initially-hidden").slideToggle();
   });
+
+  $("#green").click(function () {
+    $("body").removeClass("yellow-background red-background");
+    $("body").addClass("green-background");
+  });
+
+  $("#yellow").click(function () {
+    $("body").removeClass("green-background red-background");
+    $("body").addClass("yellow-background");
+  });
+
+  $("#red").click(function () {
+    $("body").removeClass("green-background yellow-background");
+    $("body").addClass("red-background");
+  });
+
+  $("#white").click(function () {
+    $("body").removeClass("yellow-background red-background green-background");
+  })
+
+
 });
